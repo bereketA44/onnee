2ad2db5bae438ca29cd34dfb8b868ebebf20949e
bereketA44 <bereketababub@gmail.com>
2023-05-05 20:46:07 -0500

    before postgres

--- src/main/java/com/app/onnee/controller/AnalyticsController.java
+++ src/main/java/com/app/onnee/controller/AnalyticsController.java
@@ -1,2 +1,20 @@
-package com.app.onnee.controller;public class AnalyticsController {
+package com.app.onnee.controller;
+
+import com.app.onnee.service.AnalyticsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AnalyticsController {
+
+    @Autowired
+    AnalyticsService analyticsService;
+
+    @GetMapping(path = "/analytics")
+    public ResponseEntity<?> getAnalytics() {
+        return new ResponseEntity<>(analyticsService.getUserAnalytics(), HttpStatus.OK);
+    }
 }
--- src/main/java/com/app/onnee/service/AnalyticsService.java
+++ src/main/java/com/app/onnee/service/AnalyticsService.java
@@ -1,2 +1,41 @@
-package com.app.onnee.service;public class AnalyticsService {
+package com.app.onnee.service;
+
+
+import com.app.onnee.dto.AnalyticsDTO;
+import com.app.onnee.model.Profile;
+import com.app.onnee.model.User;
+import com.app.onnee.repository.ProfileRepository;
+import com.app.onnee.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Service
+public class AnalyticsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    ProfileRepository profileRepository;
+
+    public AnalyticsDTO getUserAnalytics() {
+        Map<String, Object> data = new HashMap<>();
+        List<User> users = userRepository.findAll();
+        List<Profile> profiles = profileRepository.findAll();
+        data.put("Totoal users", users.size());
+        data.put("totoal profiles", profiles.size());
+        data.put("Users with profile", profiles.stream().map(Profile::getUser));
+        List<Long> userIdsWithProfile = profiles.stream().map(Profile::getId).toList();
+        data.put("Users with no profile", users.stream().filter(user -> {
+            return !userIdsWithProfile.contains(user.getId());
+        }));
+        AnalyticsDTO analyticsDTO = new AnalyticsDTO("Data needed for analytics", data);
+        return analyticsDTO;
+    }
 }
